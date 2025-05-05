feb5228dd3a55fd059403d29ca2e51153491e488
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-05 11:49:42 +0100

    Fetch image's author

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+const imgAuthor = document.getElementById("img-author");
+
 fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=seoul")
     .then (res => res.json())
-    .then (data => {
-        document.body.style.backgroundImage = `url("${data.urls.full}")`
-    })
+    .then (image => {
+        document.body.style.backgroundImage = `url("${image.urls.regular}")`
+        console.log(image)
+        if (image.user.instagram_username) {
+            imgAuthor.innerHTML = `
+            <p>Credit: @${image.user.instagram_username}</p>
+        `
+        } else {
+            imgAuthor.innerHTML = `
+            <p>Credit: ${image.user.name}</p>
+        `
+        }
+        
+    });
