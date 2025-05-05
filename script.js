261df4658b50b8f9d7202984a2ba788bcc57aeb3
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-05 13:41:38 +0100

    Korean API
    Will work on the fetch later

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&qu
     .then (res => res.json())
     .then (image => {
         document.body.style.backgroundImage = `url(${image.urls.regular})`
-        console.log(image.urls.regular)
         if (image.user.instagram_username) {
             imgAuthor.innerHTML = `
             <p>Credit: @${image.user.instagram_username}</p>
@@ -15,9 +14,18 @@ fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&qu
         `
         }
     })
-    .catch(err => {
+    .catch (err => {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1628008335819-7175b35fa4f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDY0NDU2Mjh8&ixlib=rb-4.0.3&q=80&w=1080)`
         imgAuthor.innerHTML = `
             <p>Credit: @shining.shot</p>
         `
+    });
+
+fetch ("https://krdict.korean.go.kr/api/search")
+    .then (res => res.json())
+    .then (data => {
+        
+    })
+    .catch (err => {
+
     });
