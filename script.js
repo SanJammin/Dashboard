be8b291131d8d7b1f4a1e7dff9e446610209b2e3
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-05 11:40:53 +0100

    link stylesheet properly

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=seoul")
     .then (res => res.json())
     .then (data => {
-        document.body.style.backgroundImage = `url("${data.urls.regular}")`
+        document.body.style.backgroundImage = `url("${data.urls.full}")`
     })
