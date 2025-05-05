34ceb5faf8299d44701e88da2a2aa06033fd7c37
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-05 15:01:32 +0100

    Add weather API

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const imgAuthor = document.getElementById("img-author");
 const currentTime = document.getElementById("current-time");
+const weatherDiv = document.getElementById("weather-div");
 
 function updateTime() {
     const date = new Date();
@@ -7,7 +8,7 @@ function updateTime() {
     const minutes = date.getMinutes().toString().padStart(2, "0");
     currentTime.innerHTML = `
     <h1>${hours}:${minutes}</h1>
-    `
+    `;
 };
 
 updateTime();
@@ -15,30 +16,49 @@ setInterval(updateTime, 1000);
 
 fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=seoul")
     .then (res => res.json())
-    .then (image => {
-        document.body.style.backgroundImage = `url(${image.urls.regular})`
-        if (image.user.instagram_username) {
+    .then (data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        if (data.user.instagram_username) {
             imgAuthor.innerHTML = `
-            <p>Credit: @${image.user.instagram_username}</p>
-        `
+            <p>Credit: @${data.user.instagram_username}</p>
+        `;
         } else {
             imgAuthor.innerHTML = `
-            <p>Credit: ${image.user.name}</p>
-        `
+            <p>Credit: ${data.user.name}</p>
+        `;
         }
     })
     .catch (err => {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1628008335819-7175b35fa4f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDY0NDU2Mjh8&ixlib=rb-4.0.3&q=80&w=1080)`
         imgAuthor.innerHTML = `
             <p>Credit: @shining.shot</p>
-        `
+        `;
     });
 
 fetch ("https://krdict.korean.go.kr/api/search")
-    .then (res => res.json())
+    .then (res => {
+        if (!res.ok) throw Error("Data not available");
+        return res.json();
+    })
     .then (data => {
         
     })
-    .catch (err => {
+    .catch (err => console.error(err));
 
-    });
+navigator.geolocation.getCurrentPosition((position) => {
+    fetch (`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then (res => {
+            if (!res.ok) throw Error("Weather data not available");
+            return res.json();
+        })
+        .then (data => {
+            weatherDiv.innerHTML = `
+                <div class="weather-styling">
+                    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+                    <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                    <p class="weather-city">${data.name}</p>
+                </div>
+            `;
+        })
+        .catch (err => console.error(err));
+});
