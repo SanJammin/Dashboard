2b41141eee939a4e7d7c89a2dcaa90091433edc1
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-06 19:37:26 +0100

    Final refactor
    Remove definition API

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-import { getRandomWordFromFile, fetchXMLDefinition, translateWord } from "./utils.js";
+import { getRandomWordFromFile, translateWord } from "./utils.js";
 import { CONFIG } from "./config.js";
 
+const {AZURE_KEY, AZURE_REGION, AZURE_ENDPOINT } = CONFIG;
 const imgAuthor = document.getElementById("img-author");
 const currentTime = document.getElementById("current-time");
 const weatherDiv = document.getElementById("weather-div");
 const wordContainer = document.getElementById("korean-word");
-
-// API Consts
-const { API_KEY, AZURE_KEY, AZURE_REGION, AZURE_ENDPOINT } = CONFIG;
-
 
 function updateTime() {
     const date = new Date();
@@ -20,54 +17,51 @@ function updateTime() {
 updateTime();
 setInterval(updateTime, 1000);
 
+displayWord();
+
 async function displayWord() {
     try {
         const word = await getRandomWordFromFile();
         const translation = await translateWord(word, AZURE_KEY, AZURE_REGION, AZURE_ENDPOINT);
-        const definition = await fetchXMLDefinition(word, API_KEY);
-
         wordContainer.innerHTML = `
-            <span class="korean">${word}</span>
-            <span class="english-hover">${translation}</span>
+                <span class="korean">${word}</span>
+                <span class="english-hover">${translation}</span>
         `;
-        wordContainer.setAttribute("data-definition", definition || "");
     } catch (error) {
-        console.error("Error displaying wordL", error);
+        console.error("Error displaying word:", error);
         wordContainer.textContent = "Error loading word";
-        wordContainer.setAttribute("data-definition", "");
     }
 }
 
-displayWord();
 
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=seoul")
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`;
-        imgAuthor.innerHTML = data.user.instagram_username
-            ? `<p>Credit: @${data.user.instagram_username}</p>`
-            : `<p>Credit: ${data.user.name}</p>`;
+        imgAuthor.innerHTML = `
+            <p>Credit: ${data.user.instagram_username ? `@${data.user.instagram_username}` : data.user.name}</p>
+        `
     }) 
-    .catch(err => {
+    .catch(() => {
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1628008335819-7175b35fa4f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NDY0NDU2Mjh8&ixlib=rb-4.0.3&q=80&w=1080)`;
         imgAuthor.innerHTML = `<p>Credit: @shining.shot</p>`;
     });
 
-    navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`)
-            .then(res => {
-                if (!res.ok) throw new Error("Weather data not available");
-                return res.json();
-            })
-            .then(data => {
-                weatherDiv.innerHTML = `
-                    <div class="weather-styling">
-                        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
-                        <p class="weather-temp">${Math.round(data.main.temp)}º</p>
-                        <p class="weather-city">${data.name}</p>
-                    </div>
-                `;
-            })
-            .catch(err => console.error(err));
-    })
+navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) throw new Error("Weather data not available");
+            return res.json();
+        })
+        .then(data => {
+            weatherDiv.innerHTML = `
+                <div class="weather-styling">
+                    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />
+                    <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                    <p class="weather-city">${data.name}</p>
+                </div>
+            `;
+        })
+        .catch(err => console.error(err));
+});
--- utils.js
+++ utils.js
@@ -7,21 +7,8 @@ export function getRandomWordFromFile(filepath = "./data/ko_50k.txt") {
         });
 }
 
-export function fetchXMLDefinition(word, API_KEY) {
-    const url = `https://krdict.korean.go.kr/api/search?key=${API_KEY}&q=${encodeURIComponent(word)}`;
-    return fetch(url)
-        .then(res => res.text())
-        .then(str => {
-            const parser = new DOMParser();
-            const xml = parser.parseFromString(str, "application/xml");
-            const definitionNode = xml.querySelector("definition");
-            return definitionNode ? definitionNode.textContent : null;
-        });
-}
-
-export async function translateWord(word, AZURE_KEY, AZURE_REGION, AZURE_ENDPOINT) {
+export async function translateWord(text, AZURE_KEY, AZURE_REGION, AZURE_ENDPOINT) {
     const url = `${AZURE_ENDPOINT}/translate?api-version=3.0&from=ko&to=en`;
-
     try {
         const res = await fetch(url, {
             method: "POST",
@@ -30,9 +17,8 @@ export async function translateWord(word, AZURE_KEY, AZURE_REGION, AZURE_ENDPOIN
                 "Ocp-Apim-Subscription-Region": AZURE_REGION,
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify([{ Text: word }])
+            body: JSON.stringify([{ Text: text }])
         });
-
         if (!res.ok) throw new Error(`Translation failed with status ${res.status}`);
         const data = await res.json();
         return data[0]?.translations[0].text || "Translation unavailable";
